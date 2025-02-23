689a49bbe733be2f71f393dcaf3c64e5c5decfdc
Viren Patil <viren.p2000@gmail.com>
2025-02-23 14:49:45 -0800

    Modified experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -8,13 +8,13 @@ export const experiences = [
   {
     id: 2,
     title: "Technical Analyst",
-    company: "Citicorp Services India Private Ltd.",
+    company: "Citibank (Citicorp Services India Private Ltd.)",
     duration: "(July 2022 - June 2023)"
   },
   {
     id: 3,
     title: "Summer Analyst Intern",
-    company: "Citicorp Services India Private Ltd.",
+    company: "Citibank (Citicorp Services India Private Ltd.)",
     duration: "(May 2021 - July 2021)"
   }
 ]
