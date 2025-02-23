df74d9f3db3c91ed460db155754d7838ec118a56
Viren Patil <viren.p2000@gmail.com>
2025-02-23 14:50:43 -0800

    Changed phone number

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Developer",
   description: "I am Viren Patil, a Master's student in Computer Science at the University of Southern California, set to graduate in May 2025. My passion lies in building innovative solutions, whether through coding or other creative avenues. I thrive on the challenge of debugging and have recently delved into the realm of large language models (LLMs) to discover optimal applications for pre-trained models. Outside of my academic pursuits, I enjoy watching movies and playing games on my PC. When faced with a roadblock in my projects, I find that a hot shower often provides the relaxation and clarity needed to approach problems from a fresh perspective.",
   email: 'viren.p2000@gmail.com',
-  phone: '+1 213-663-7219',
+  phone: '+1 213-348-7219',
   address: 'Los Angeles, CA, United States - 90007',
   github: 'https://github.com/Viren-Patil',
   facebook: 'https://www.facebook.com/viren.patil.336',
