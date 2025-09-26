feeccf2d98e83eed42f56e1ddff390a6d3f04177
Viren Patil <viren.p2000@gmail.com>
2025-09-26 16:02:45 -0700

    Changed twitter logo

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -68,7 +68,7 @@ function HeroSection() {
               target='_blank'
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
-              <FaTwitterSquare size={30} />
+              <FaXTwitter size={30} />
             </Link>
           </div>
 
