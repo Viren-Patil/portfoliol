633e202dd2c33449953835880df01ffcd704bb72
Viren Patil <viren.p2000@gmail.com>
2025-11-14 10:32:25 -0800

    Updated experience and skills in hero section

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -118,6 +118,12 @@ function HeroSection() {
               <div className="ml-4 lg:ml-8 mr-2">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`['`}</span>
+                <span className="text-amber-300">React</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Node.js</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Express.js</span>
+                <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Angular</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Django</span>
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,19 +1,31 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Research Assistant',
+    company: "USC Viterbi School of Engineering",
+    duration: "(September 2025 - Present)"
+  },
+  {
+    id: 2,
+    title: 'Software Engineer - Backend Systems',
+    company: "Easley Dunn Productions",
+    duration: "(July 2025 - October 2025)"
+  },
+  {
+    id: 3,
     title: 'AI Engineer',
     company: "Radical AI",
     duration: "(June 2024 - August 2024)"
   },
   {
-    id: 2,
-    title: "Technical Analyst",
+    id: 4,
+    title: "Software Engineer",
     company: "Citibank (Citicorp Services India Private Ltd.)",
     duration: "(July 2022 - June 2023)"
   },
   {
-    id: 3,
-    title: "Summer Analyst Intern",
+    id: 5,
+    title: "Software Engineer Intern",
     company: "Citibank (Citicorp Services India Private Ltd.)",
     duration: "(May 2021 - July 2021)"
   }
